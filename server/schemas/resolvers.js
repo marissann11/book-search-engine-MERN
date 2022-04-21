553341b5e39e66fba01f0e7c9d5c81eee11fbe60
marissann11 <wilhite.marissa@gmail.com>
2022-04-21 12:59:40 -0400

    book input added and re-install of node modules

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,20 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async ({ username }) => {
-      return User.findOne({ username })
-        .select('-__v -password')
-        .populate('savedBooks');
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          '-__v -password'
+        );
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
     },
   },
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -33,19 +38,14 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: async (parent, args, context) => {
+    saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
-        const book = await Book.create({
-          ...args,
-          username: context.user.username,
-        });
-
-        await User.findByIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $push: { savedBooks: book.bookId } },
+          { $push: { savedBooks: bookData } },
           { new: true }
         );
-        return book;
+        return updatedUser;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
@@ -53,7 +53,7 @@ const resolvers = {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { savedBooks: bookId } },
+          { $pull: { savedBooks: { bookId } } },
           { new: true }
         );
         return updatedUser;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,13 +5,6 @@ const typeDefs = gql`
     me: User
   }
 
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(): User
-    removeBook(bookId: ID!): User
-  }
-
   type User {
     _id: ID
     username: String
@@ -29,10 +22,26 @@ const typeDefs = gql`
     link: String
   }
 
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
   type Auth {
     token: ID!
     user: User
-    }
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
